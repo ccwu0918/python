be05d8d2dd0e774f4bcda40aca97cb33ef6b135c
Chia-Chun Wu <ccwu0918@gmail.com>
2019-05-14 11:48:45 +0800

    Add files via upload

---
+++ S13913001-Ex01.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+start = input("你今天幾點開始工作?") 
+end = input("現在幾點?")
+print("你已經工作了", (int(end)-int(start)), " 小時!")
